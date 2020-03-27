b750057513b49ce635ddd0723bbef6899710926c
NiNyomanSusiAstuti <komangsusiastutii@gmail.com>
2020-03-27 13:26:02 +0530

    first commit

---
+++ laravel_susi/resources/views/admin/toko.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.tes')
+@section('content')
+<table class="table bordered">
+                     <tr>
+                         <td width='300'><h4>Biodata</h4></td>
+                         <td width='300'><h4>Studi Kasus</h4></td>
+                     </tr>
+                     <tr>
+                         <td>Nama   : Ni Nyoman Susi Astuti</td>
+                         <td>Judul  : Sistem Toko Kelontong</td>
+                     </tr>
+                     <tr rowspan='2'>
+                         <td>NIM   : 18150051113</td>
+                         <td>Penjelasan: Sistem Toko Kelontong merupakan suatu toko yang mudah diakses, toko semacam ini umumnya berlokasi dijalan raya</td>
+                     </tr>
+                     <tr>
+                         <td>Program Studi: Pendidikan Teknik Informatika</td>
+                         <td></td>
+                     </tr>
+
+                 </table>
+@endsection
+
+
